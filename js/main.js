d0868b05d0cdd369c4ead7e1e092342548016667
Sethmi Kachchakaduge <snkachchakaduge@gmail.com>
2020-03-27 20:01:01 -0700

    ww
    w

--- js/main.js
+++ js/main.js
@@ -40,7 +40,13 @@
 	// Scrollax
    $.Scrollax();
 
-
+$( document ).ready(function() {
+         $('#headerVideoLink').magnificPopup({
+          type:'inline',
+          midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+        });
+          
+        });
 
    // Burger Menu
 	var burgerMenu = function() {
